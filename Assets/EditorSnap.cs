af777a7d91be64f6e64b0449962bbac8f39d98dc
Anthony Mendez <anthonymendez9@gmail.com>
2018-05-24 14:42:49 -0400

    S5:L123: Added snapping within the editor for quad cubes. Adjustable between 1f and 20f.

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+    
+    [Tooltip("GameObject will snap to positions of factors of this value.")][Range(1f, 20f)] [SerializeField] float
+        gridSize = 10f;
+
+    Vector3 snapPos;
+
+	void Update () {
+        PerformSnapPosition();
+    }
+
+    private void PerformSnapPosition() {
+        CalculateSnapPosition();
+        SetSnapPosition();
+    }
+
+    private void SetSnapPosition() {
+        // Set position to snap position
+        transform.position = snapPos;
+    }
+
+    private void CalculateSnapPosition() {
+        // Calculate closest position of a factor of 10f
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+    }
+}
