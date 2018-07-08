9438950da13485ac1e0655f3b53b085f58658c01
Anthony Mendez <anthonymendez9@gmail.com>
2018-07-08 15:35:52 -0400

    Towers check for distance before shooting.

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour {
 
+    [Header("Tower Properties")]
+    [SerializeField] float maxFiringDistance = 10f;
+
+    [Header("Targeting Properties")]
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy; // TODO Change based on enemy.
-	
+
+    [Header("Components")]
+    [SerializeField] ParticleSystem bulletParticleSystem;
+
 	// Update is called once per frame
 	void Update () {
-        objectToPan.LookAt(targetEnemy);
+        if (targetEnemy && IsEnemyWithinRange()) {
+            objectToPan.LookAt(targetEnemy);
+            Shoot(true);
+        } else {
+            Shoot(false);
+        }
 	}
+
+    private bool IsEnemyWithinRange() {
+        float distanceToEnemy = Vector3.Distance(objectToPan.position, targetEnemy.position);
+        return maxFiringDistance >= distanceToEnemy;
+    }
+
+    private void Shoot(bool isActive) {
+        ParticleSystem.EmissionModule emissionModule = bulletParticleSystem.emission;
+        emissionModule.enabled = isActive;
+    }
 }
